2db9738cb7ee0a23939827dd1b70fa109d8f429b
RadhikaRamalingam <33268841+RadhikaRamalingam@users.noreply.github.com>
2017-10-31 19:10:34 -0400

    Add files via upload

---
+++ maximize.rb
@@ -0,0 +1,9 @@
+WebDriver driver = new FirefoxDriver();
+ driver.manage().window().maximize();
+ ==========================================================
+ driver.Manage().Window.Maximize(); 
+ ==========================================================
+ Call window as a function.
+
+driver.manage().window().maximize();
+=================================================================
